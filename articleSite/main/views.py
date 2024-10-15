013404a3d93a9ba436b4229591c91ecd57aecca0
LegroQ <dampik228@yandex.ru>
2024-10-15 19:01:47 +0300

    Small fixes

--- articleSite/main/views.py
+++ articleSite/main/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     data = {
-        'title': 'Main page',
-        'values': ['Some', 'Hello', '123']
+        'title': 'Главная страница',
     }
     return render(request, 'main/index.html', data)
 
